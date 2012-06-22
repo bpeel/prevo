2a72bba7ce93bf81548a9c05b06a13c96101544b
Neil Roberts <bpeeluk@yahoo.co.uk>
2012-06-22 21:05:01 +0100

    Initial source
    This is just the source generated by the android tool

---
+++ src/uk/co/busydoingnothing/prevo/Prevo.java
@@ -0,0 +1,15 @@
+package uk.co.busydoingnothing.prevo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Prevo extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
