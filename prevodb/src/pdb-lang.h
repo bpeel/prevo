88ab1645ad613970e31295e2ca9a6707e8677b52
Neil Roberts <bpeeluk@yahoo.co.uk>
2012-06-30 13:42:15 +0100

    Add a GPLv2 notice to all of the source files
    This adds a copyright notice for GPLv2 to all of the source files.
    
    Only GPLv2 is used (not later versions) because that is the license of
    ReVo itself so it's potentially a derived work and should probably
    have the same license.

--- prevodb/src/pdb-lang.h
+++ prevodb/src/pdb-lang.h
@@ -1,3 +1,20 @@
+/*
+ * PReVo - A portable version of ReVo for Android
+ * Copyright (C) 2012  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef PDB_LANG_H
 #define PDB_LANG_H
 
